ea32e8d3d723d58a0f6e2cc80c8d6edf42f2cfb9
compugeen <support@compugeen.com>
2022-02-25 12:37:51 +0100

    first commit

--- routes/users.js
+++ routes/users.js
@@ -3,51 +3,51 @@ const bcrypt = require("bcryptjs");
 const router = require("express").Router();
 
 //update user
-// router.put(":/id", async(req, res) => {
-//     if (req.body.userId === req.params.id || req.body.isAdmin) {
-//         if(req.body.password) {
-//             try{
-//                 const salt = await bcrypt.genSalt(10);
-//                 req.body.password = await bcrypt.hash(req.body.password, salt);
-//             }catch(err){
-//                 return res.status(500).json(err);
-//             }
-//         }
-//         try{
-//             const user = await User.findByIdAndUpdate(req.params.id, {
-//                 $set: req.body,
-//             });
-//             res.status(200).json("Account has been updated successfully");
-//         }catch(err) {
-//             return res.status(500).json(err);
-//         }
-//     } else {
-//         return res.status(403).json("You can update only your account");
-//     }
-// });
-
-router.put("/:id", async (req, res) => {
-  if (req.body.userId === req.params.id) {
-    if (req.body.password) {
-      const salt = await bcrypt.genSalt(10);
-      req.body.password = await bcrypt.hash(req.body.password, salt);
+router.put(":/id", async(req, res) => {
+    if (req.body.userId === req.params.id || req.body.isAdmin) {
+        if(req.body.password) {
+            try{
+                const salt = await bcrypt.genSalt(10);
+                req.body.password = await bcrypt.hash(req.body.password, salt);
+            }catch(err){
+                return res.status(500).json(err);
+            }
+        }
+        try{
+            const user = await User.findByIdAndUpdate(req.params.id, {
+                $set: req.body,
+            });
+            res.status(200).json("Account has been updated successfully");
+        }catch(err) {
+            return res.status(500).json(err);
+        }
+    } else {
+        return res.status(403).json("You can update only your account");
     }
-    try {
-      const updatedUser = await User.findByIdAndUpdate(
-        req.params.id,
-        {
-          $set: req.body,
-        },
-        { new: true }
-      );
-      res.status(200).json(updatedUser);
-    } catch (err) {
-      res.status(500).json(err);
-    }
-  } else {
-    res.status(401).json("you can update only your account!");
-  }
 });
+
+// router.put("/:id", async (req, res) => {
+//   if (req.body.userId === req.params.id) {
+//     if (req.body.password) {
+//       const salt = await bcrypt.genSalt(10);
+//       req.body.password = await bcrypt.hash(req.body.password, salt);
+//     }
+//     try {
+//       const updatedUser = await User.findByIdAndUpdate(
+//         req.params.id,
+//         {
+//           $set: req.body,
+//         },
+//         { new: true }
+//       );
+//       res.status(200).json(updatedUser);
+//     } catch (err) {
+//       res.status(500).json(err);
+//     }
+//   } else {
+//     res.status(401).json("you can update only your account!");
+//   }
+// });
 
 //delete user
 router.delete("/:id", async (req, res) => {
